8e565f9442ee4105af5eb0b2bfbc9cec8d3584e4
ixen <ixen@copyhandler.com>
2006-08-01 20:11:23 +0000

    Another correction of the previous mistake.

--- ext/libicpf/src/cfg.h
+++ ext/libicpf/src/cfg.h
@@ -66,7 +66,7 @@ class config;
 
 extern config *__g_cfg;
 
-ICPFTEMPL_EXTERN template class LIBICPF_API callback2<ptr_t, ulong_t, ptr_t>;
+ICPFTEMPL_EXTERN template class LIBICPF_API callback2<void, ulong_t, ptr_t>;
 
 /** \brief Structure contain information about one property.
  *
