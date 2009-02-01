503252efefd3fa1ee28e07c8a6402760785a372e
ixen <ixen@copyhandler.com>
2009-02-01 21:57:12 +0000

    Fixed zip version generation script.

--- scripts/makepkg.bat
+++ scripts/makepkg.bat
@@ -9,7 +9,7 @@ set OutputDir=%CD%
 
 rem ---------------------------------------------------
 echo Performing a cleanup...
-cd %tmp%
+rem cd %tmp%
 if exist copyhandler (
 	rmdir /S /Q copyhandler
 	if exist copyhandler (
@@ -128,9 +128,11 @@ if errorlevel 1 (
 
 rem -------------
 rem We are in scripts/
+cd ..
+
 rem 1st phase - loose files
 
-zip -r -j -9 %OutputDir%\ch32.zip "copyhandler\bin\release\ch.exe" "copyhandler\License.txt" "copyhandler\bin\release\chext.dll" "copyhandler\bin\release\libicpf32u.dll" "copyhandler\bin\release\libchcore32u.dll" "copyhandler\bin\release\libictranslate32u.dll" "copyhandler\bin\release\ictranslate.exe" "C:\Program Files\Microsoft Visual Studio 9.0\VC\redist\x86\Microsoft.VC90.CRT\*" "C:\Program Files\Microsoft Visual Studio 9.0\VC\redist\x86\Microsoft.VC90.MFC\*" "C:\Program Files\Microsoft Visual Studio 9.0\Common7\IDE\Remote Debugger\x86\dbghelp.dll"
+zip -r -j -9 %OutputDir%\ch32.zip "bin\release\ch.exe" "License.txt" "bin\release\chext.dll" "bin\release\libicpf32u.dll" "bin\release\libchcore32u.dll" "bin\release\libictranslate32u.dll" "bin\release\ictranslate.exe" "C:\Program Files\Microsoft Visual Studio 9.0\VC\redist\x86\Microsoft.VC90.CRT\*" "C:\Program Files\Microsoft Visual Studio 9.0\VC\redist\x86\Microsoft.VC90.MFC\*" "C:\Program Files\Microsoft Visual Studio 9.0\Common7\IDE\Remote Debugger\x86\dbghelp.dll"
 
 if errorlevel 1 (
 	echo Could not create win32 zip archive.
@@ -152,7 +154,7 @@ cd ..\..
 
 rem 1st phase - loose files
 
-zip -r -j -9 %OutputDir%\ch64.zip "copyhandler\bin\release\ch64.exe" "copyhandler\License.txt" "copyhandler\bin\release\chext64.dll" "copyhandler\bin\release\libicpf64u.dll" "copyhandler\bin\release\libchcore64u.dll" "copyhandler\bin\release\libictranslate64u.dll" "copyhandler\bin\release\ictranslate64.exe" "C:\Program Files\Microsoft Visual Studio 9.0\VC\redist\x86\Microsoft.VC90.CRT\*" "C:\Program Files\Microsoft Visual Studio 9.0\VC\redist\amd64\Microsoft.VC90.MFC\*" "C:\Program Files\Microsoft Visual Studio 9.0\Common7\IDE\Remote Debugger\x64\dbghelp.dll"
+zip -r -j -9 %OutputDir%\ch64.zip "bin\release\ch64.exe" "License.txt" "bin\release\chext64.dll" "bin\release\libicpf64u.dll" "bin\release\libchcore64u.dll" "bin\release\libictranslate64u.dll" "bin\release\ictranslate64.exe" "C:\Program Files\Microsoft Visual Studio 9.0\VC\redist\x86\Microsoft.VC90.CRT\*" "C:\Program Files\Microsoft Visual Studio 9.0\VC\redist\amd64\Microsoft.VC90.MFC\*" "C:\Program Files\Microsoft Visual Studio 9.0\Common7\IDE\Remote Debugger\x64\dbghelp.dll"
 
 if errorlevel 1 (
 	echo Could not create win64 zip archive.
