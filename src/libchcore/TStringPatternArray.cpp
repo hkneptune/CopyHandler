5ccf162f73d05a43bbe5cb7c01d16d07b6b0d4d9
ixen <ixen@copyhandler.com>
2015-10-06 16:11:58 +0000

    Added missing files (CH-165)

---
+++ src/libchcore/TStringPatternArray.cpp
@@ -0,0 +1,125 @@
+// ============================================================================
+//  Copyright (C) 2001-2015 by Jozef Starosczyk
+//  ixen@copyhandler.com
+//
+//  This program is free software; you can redistribute it and/or modify
+//  it under the terms of the GNU Library General Public License
+//  (version 2) as published by the Free Software Foundation;
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU Library General Public
+//  License along with this program; if not, write to the
+//  Free Software Foundation, Inc.,
+//  59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+// ============================================================================
+#include "stdafx.h"
+#include "TStringPatternArray.h"
+#include "TCoreException.h"
+#include "ErrorCodes.h"
+#include "TStringArray.h"
+
+BEGIN_CHCORE_NAMESPACE
+
+TStringPatternArray::TStringPatternArray()
+{
+}
+
+TStringPatternArray::~TStringPatternArray()
+{
+}
+
+void TStringPatternArray::Add(const TStringPattern& strPattern)
+{
+	m_vPatterns.push_back(strPattern);
+}
+
+void TStringPatternArray::InsertAt(size_t stIndex, const TStringPattern& strPattern)
+{
+	if (stIndex > m_vPatterns.size())
+		THROW_CORE_EXCEPTION(eErr_BoundsExceeded);
+
+	m_vPatterns.insert(m_vPatterns.begin() + stIndex, strPattern);
+}
+
+void TStringPatternArray::SetAt(size_t stIndex, const TStringPattern& strPattern)
+{
+	if (stIndex >= m_vPatterns.size())
+		THROW_CORE_EXCEPTION(eErr_BoundsExceeded);
+
+	m_vPatterns[stIndex] = strPattern;
+}
+
+void TStringPatternArray::RemoveAt(size_t stIndex)
+{
+	if (stIndex >= m_vPatterns.size())
+		THROW_CORE_EXCEPTION(eErr_BoundsExceeded);
+
+	m_vPatterns.erase(m_vPatterns.begin() + stIndex);
+}
+
+void TStringPatternArray::Clear()
+{
+	m_vPatterns.clear();
+}
+
+const TStringPattern& TStringPatternArray::GetAt(size_t stIndex) const
+{
+	if (stIndex >= m_vPatterns.size())
+		THROW_CORE_EXCEPTION(eErr_BoundsExceeded);
+
+	return m_vPatterns[stIndex];
+}
+
+size_t TStringPatternArray::GetCount() const
+{
+	return m_vPatterns.size();
+}
+
+bool TStringPatternArray::MatchesAny(const TString& strTextToMatch) const
+{
+	for (const TStringPattern& pattern : m_vPatterns)
+	{
+		if (pattern.Matches(strTextToMatch))
+			return true;
+	}
+
+	return false;
+}
+
+bool TStringPatternArray::MatchesAll(const TString& strTextToMatch) const
+{
+	for (const TStringPattern& pattern : m_vPatterns)
+	{
+		if (!pattern.Matches(strTextToMatch))
+			return false;
+	}
+
+	return true;
+}
+
+void TStringPatternArray::FromStringArray(const TStringArray& arrSerializedPatterns)
+{
+	m_vPatterns.clear();
+
+	for (size_t stIndex = 0; stIndex < arrSerializedPatterns.GetCount(); ++stIndex)
+	{
+		m_vPatterns.push_back(TStringPattern::CreateFromSerializedString(arrSerializedPatterns.GetAt(stIndex)));
+	}
+}
+
+TStringArray TStringPatternArray::ToStringArray() const
+{
+	TStringArray arrSerialized;
+	for (const TStringPattern& pattern : m_vPatterns)
+	{
+		arrSerialized.Add(pattern.ToSerializedString());
+	}
+
+	return arrSerialized;
+}
+
+END_CHCORE_NAMESPACE
---
+++ src/libchcore/TStringPatternArray.h
@@ -0,0 +1,60 @@
+// ============================================================================
+//  Copyright (C) 2001-2015 by Jozef Starosczyk
+//  ixen@copyhandler.com
+//
+//  This program is free software; you can redistribute it and/or modify
+//  it under the terms of the GNU Library General Public License
+//  (version 2) as published by the Free Software Foundation;
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU Library General Public
+//  License along with this program; if not, write to the
+//  Free Software Foundation, Inc.,
+//  59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+// ============================================================================
+#ifndef __TSTRINGPATTERNARRAY_H__
+#define __TSTRINGPATTERNARRAY_H__
+
+#include "libchcore.h"
+#include "TStringPattern.h"
+
+BEGIN_CHCORE_NAMESPACE
+
+class LIBCHCORE_API TStringPatternArray
+{
+public:
+	TStringPatternArray();
+	~TStringPatternArray();
+
+	// general api
+	void Add(const TStringPattern& strPattern);
+	void InsertAt(size_t stIndex, const TStringPattern& strPattern);
+	void SetAt(size_t stIndex, const TStringPattern& strPattern);
+	void RemoveAt(size_t stIndex);
+	void Clear();
+
+	const TStringPattern& GetAt(size_t stIndex) const;
+	size_t GetCount() const;
+
+	// pattern api
+	bool MatchesAny(const TString& strTextToMatch) const;
+	bool MatchesAll(const TString& strTextToMatch) const;
+
+	// serialization
+	void FromStringArray(const TStringArray& arrSerializedPatterns);
+	TStringArray ToStringArray() const;
+
+private:
+#pragma warning(push)
+#pragma warning(disable: 4251)
+	std::vector<TStringPattern> m_vPatterns;
+#pragma warning(pop)
+};
+
+END_CHCORE_NAMESPACE
+
+#endif
