2e2d859d8cd8feed4f2d8dc0ecf75bf22dcb2c99
ixen <ixen@copyhandler.com>
2015-12-22 21:22:33 +0000

    Updated scripts to build boost 1.60 with WinXP support (CH-201).

--- tools/boost-build64.bat
+++ tools/boost-build64.bat
@@ -8,4 +8,4 @@ set CL=/D_USING_V110_SDK71_;%CL%
 set LIB=%ProgramFiles(x86)%\Microsoft SDKs\Windows\7.1A\Lib\x64;%LIB%
 set LINK=/SUBSYSTEM:CONSOLE,5.02 %LINK%
 
-bjam --toolset=msvc-12.0 --link=shared --threading=multi --runtime-link=shared address-model=64 define=_BIND_TO_CURRENT_VCLIBS_VERSION --build-type=complete --stagedir=lib-12.0\x64 stage
+b2 -j 2 --toolset=msvc-14.0 --link=static --threading=multi --runtime-link=shared address-model=64 define=_BIND_TO_CURRENT_VCLIBS_VERSION define=BOOST_USE_WINAPI_VERSION=0x0501 --build-type=complete --stagedir=lib-14.0\x64 stage
