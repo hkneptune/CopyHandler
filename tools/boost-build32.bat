146b40c45c5406090ee3979f12422e04f1f36ce2
ixen <ixen@copyhandler.com>
2016-03-18 21:49:55 +0100

    Updated boost build scripts to disable SSE for x86_32 builds (CH-239).

--- tools/boost-build32.bat
+++ tools/boost-build32.bat
@@ -8,4 +8,4 @@ set CL=/D_USING_V110_SDK71_;%CL%
 set LIB=%ProgramFiles(x86)%\Microsoft SDKs\Windows\7.1A\Lib;%LIB%
 set LINK=/SUBSYSTEM:CONSOLE,5.01 %LINK%
 
-b2 -j 2 --toolset=msvc-14.0 --link=static --threading=multi --runtime-link=shared address-model=32 define=_BIND_TO_CURRENT_VCLIBS_VERSION define=BOOST_USE_WINAPI_VERSION=0x0501 --build-type=complete --stagedir=lib-14.0\x32 stage
+b2 -j 4 --toolset=msvc-12.0 --link=static --threading=multi --runtime-link=shared address-model=32 define=_BIND_TO_CURRENT_VCLIBS_VERSION define=BOOST_USE_WINAPI_VERSION=0x0501 cxxflags=/arch:IA32 --build-type=complete --stagedir=lib-12.0\x32 stage
