043a0529e7bd710a92b4abda6741ffd6ef0cdafc
ixen <ixen@copyhandler.com>
2016-09-17 09:04:48 +0200

    Enabled creation of SHA256 signatures for executables (CH-263)

--- scripts/jenkins/sign-release-binaries.bat
+++ scripts/jenkins/sign-release-binaries.bat
@@ -1,4 +1,6 @@
 @echo off
 
 signtool sign /tr http://time.certum.pl /a "%WORKSPACE%\bin\release\*.dll" || exit /b 1
-signtool sign /tr http://time.certum.pl /a "%WORKSPACE%\bin\release\*.exe"
+signtool sign /tr http://time.certum.pl /a /fd sha256 /td sha256 /as "%WORKSPACE%\bin\release\*.dll" || exit /b 1
+signtool sign /tr http://time.certum.pl /a "%WORKSPACE%\bin\release\*.exe" || exit /b 1
+signtool sign /tr http://time.certum.pl /a /fd sha256 /td sha256 /as "%WORKSPACE%\bin\release\*.exe"
