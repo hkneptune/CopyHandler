d2a0a82f6248dd5f963961bedaa2e4f295028f2e
ixen <ixen@copyhandler.com>
2016-09-17 10:30:48 +0200

    Disabled signing with SHA1 completely (CH-263).

--- scripts/jenkins/sign-release-binaries.bat
+++ scripts/jenkins/sign-release-binaries.bat
@@ -1,6 +1,6 @@
 @echo off
 
-signtool sign /tr http://time.certum.pl /a "%WORKSPACE%\bin\release\*.dll" || exit /b 1
-signtool sign /tr http://time.certum.pl /a /fd sha256 /td sha256 /as "%WORKSPACE%\bin\release\*.dll" || exit /b 1
-signtool sign /tr http://time.certum.pl /a "%WORKSPACE%\bin\release\*.exe" || exit /b 1
-signtool sign /tr http://time.certum.pl /a /fd sha256 /td sha256 /as "%WORKSPACE%\bin\release\*.exe"
+rem signtool sign /tr http://time.certum.pl /a "%WORKSPACE%\bin\release\*.dll" || exit /b 1
+signtool sign /tr http://time.certum.pl /a /fd sha256 /td sha256 "%WORKSPACE%\bin\release\*.dll" || exit /b 1
+rem signtool sign /tr http://time.certum.pl /a "%WORKSPACE%\bin\release\*.exe" || exit /b 1
+signtool sign /tr http://time.certum.pl /a /fd sha256 /td sha256 "%WORKSPACE%\bin\release\*.exe"
