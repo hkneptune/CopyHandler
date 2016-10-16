591a9b3f0144389f41dd34528256f8fea8cb0238
ixen <ixen@copyhandler.com>
2016-10-16 21:27:27 +0200

    Added liblogger to the script measuring unit tests coverage (CH-206)

--- scripts/jenkins/run-unit-tests-coverage.bat
+++ scripts/jenkins/run-unit-tests-coverage.bat
@@ -10,5 +10,6 @@ del /Q "Coverage\*.*"
 
 "C:\Program Files\OpenCppCoverage\OpenCppCoverage.exe" --sources src\libchcore --working_dir "bin\Testing Debug" --export_type=cobertura:"Coverage\libchcore64ud.dll.Coverage.xml" -- tools\test_runner64.exe "libchcore64ud.dll" || exit /b 1
 "C:\Program Files\OpenCppCoverage\OpenCppCoverage.exe" --sources src\libictranslate --working_dir "bin\Testing Debug" --export_type=cobertura:"Coverage\libictranslate64ud.dll.Coverage.xml" -- tools\test_runner64.exe "libictranslate64ud.dll" || exit /b 1
+"C:\Program Files\OpenCppCoverage\OpenCppCoverage.exe" --sources src\liblogger --working_dir "bin\Testing Debug" --export_type=cobertura:"Coverage\liblogger64ud.dll.Coverage.xml" -- tools\test_runner64.exe "liblogger64ud.dll" || exit /b 1
 
 exit /b 0
