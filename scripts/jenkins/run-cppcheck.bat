37ebe302c211f40776026a04f9bd80acbf63c31a
ixen <ixen@copyhandler.com>
2016-10-16 19:26:05 +0200

    Updated cppcheck script to return more accurate results (CH-272).

--- scripts/jenkins/run-cppcheck.bat
+++ scripts/jenkins/run-cppcheck.bat
@@ -3,6 +3,6 @@ SETLOCAL
 
 SET PATH=C:\Program Files\Cppcheck;%PATH%
 
-mkdir "%WORKSPACE%\CPPCheck"
+mkdir "CPPCheck"
 
-cppcheck --enable=warning,style,performance,portability,information --suppress=cstyleCast --suppress=unusedPrivateFunction --inline-suppr --library=microsoft_sal.cfg --library=windows.cfg --std=c++11 --xml-version=2 --xml --platform=win32W --force src 2>"%WORKSPACE%\CPPCheck\cppcheck.xml"
+cppcheck --enable=warning,style,performance,portability,information --suppressions-list=scripts\jenkins\cppcheck-suppress.txt --inline-suppr --library=microsoft_sal.cfg --library=windows.cfg --std=c++11 --xml-version=2 --xml --platform=win32W -i ext -i tests --project=ch.vc140.sln 2>"CPPCheck\cppcheck.xml"
