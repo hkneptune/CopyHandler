318ace3ec2b5cf68de6aeb5a04b56c4389565e5a
ixen <ixen@copyhandler.com>
2019-03-19 22:43:56 +0100

    Enabled c++17 standard for all projects (CH-340) Fixed boost-build32.

--- tools/boost-build32.bat
+++ tools/boost-build32.bat
@@ -9,6 +9,6 @@ set CL=/D_USING_V110_SDK71_;%CL%
 set LIB=%ProgramFiles(x86)%\Microsoft SDKs\Windows\7.1A\Lib;%LIB%
 set LINK=/SUBSYSTEM:CONSOLE,5.01 %LINK%
 
-b2 -j 8 --toolset=msvc-14.1 --link=static --threading=multi --runtime-link=shared address-model=32 define=_BIND_TO_CURRENT_VCLIBS_VERSION define=BOOST_USE_WINAPI_VERSION=0x0501 --build-type=complete cxxflags="/Zc:threadSafeInit- /arch:IA32" define=BOOST_UUID_NO_SIMD --stagedir=lib-14.1\x64 stage
+b2 -j 8 --toolset=msvc-14.1 --link=static --threading=multi --runtime-link=shared address-model=32 define=_BIND_TO_CURRENT_VCLIBS_VERSION define=BOOST_USE_WINAPI_VERSION=0x0501 --build-type=complete cxxflags="/Zc:threadSafeInit- /arch:IA32" define=BOOST_UUID_NO_SIMD --stagedir=lib-14.1\x32 stage
 
 rmdir /S /Q bin.v2
