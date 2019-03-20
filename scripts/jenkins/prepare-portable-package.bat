c1fe744c16ec88e191e1eea52825ea688d3547ee
ixen <ixen@copyhandler.com>
2019-03-20 09:33:13 +0100

    Adjusted installer and jenkins scripts to use VS2017 libraries when preparing deployment (CH-340)

--- scripts/jenkins/prepare-portable-package.bat
+++ scripts/jenkins/prepare-portable-package.bat
@@ -5,7 +5,7 @@ call "%WORKSPACE%\scripts\jenkins\_get-output-dir.bat" || exit /b 1
 
 SET TmpDir=%WORKSPACE%\temp\chzip-%BUILD_NUMBER%
 
-SET VSInstallDirX86=C:\Program Files (x86)\Microsoft Visual Studio 12.0
+SET VSInstallDirX86=C:\Program Files (x86)\Microsoft Visual Studio 14.0
 
 mkdir "%TmpDir%"
 
@@ -23,9 +23,10 @@ xcopy "%WORKSPACE%\bin\release\libictranslate32u.dll" "%TmpDir%\32bit\" || exit 
 xcopy "%WORKSPACE%\bin\release\ictranslate.exe" "%TmpDir%\32bit\" || exit /B 1
 xcopy "%WORKSPACE%\bin\release\sqlite3_32.dll" "%TmpDir%\32bit\" || exit /B 1
 xcopy "%WORKSPACE%\scripts\portable_config\ch.xml" "%TmpDir%\32bit\" || exit /B 1
-xcopy "%VSInstallDirX86%\VC\redist\x86\Microsoft.VC120.CRT\*" "%TmpDir%\32bit\" || exit /B 1
-xcopy "%VSInstallDirX86%\VC\redist\x86\Microsoft.VC120.MFC\*" "%TmpDir%\32bit\" || exit /B 1
-xcopy "%VSInstallDirX86%\Common7\IDE\Remote Debugger\x86\dbghelp.dll" "%TmpDir%\32bit\" || exit /B 1
+xcopy "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\redist\x86\Microsoft.VC140.CRT\*" "%TmpDir%\32bit\" || exit /B 1
+xcopy "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\redist\x86\Microsoft.VC140.MFC\*" "%TmpDir%\32bit\" || exit /B 1
+xcopy "C:\Program Files (x86)\Windows Kits\8.1\Debuggers\x86\dbghelp.dll" "%TmpDir%\32bit\" || exit /B 1
+xcopy "C:\Program Files (x86)\Windows Kits\10\Redist\ucrt\DLLs\x86\*" "%TmpDir%\32bit\" || exit /B 1
 xcopy /E /I "%WORKSPACE%\bin\release\help" "%TmpDir%\32bit\help" || exit /B 1
 xcopy /E /I "%WORKSPACE%\bin\release\langs" "%TmpDir%\32bit\langs" || exit /B 1
 
@@ -43,9 +44,10 @@ xcopy "%WORKSPACE%\bin\release\libictranslate64u.dll" "%TmpDir%\64bit\" || exit 
 xcopy "%WORKSPACE%\bin\release\ictranslate64.exe" "%TmpDir%\64bit\" || exit /B 1
 xcopy "%WORKSPACE%\bin\release\sqlite3_64.dll" "%TmpDir%\64bit\" || exit /B 1
 xcopy "%WORKSPACE%\scripts\portable_config\ch.xml" "%TmpDir%\64bit\" || exit /B 1
-xcopy "%VSInstallDirX86%\VC\redist\x64\Microsoft.VC120.CRT\*" "%TmpDir%\64bit\" || exit /B 1
-xcopy "%VSInstallDirX86%\VC\redist\x64\Microsoft.VC120.MFC\*" "%TmpDir%\64bit\" || exit /B 1
-xcopy "%VSInstallDirX86%\Common7\IDE\Remote Debugger\x64\dbghelp.dll" "%TmpDir%\64bit\" || exit /B 1
+xcopy "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\redist\x64\Microsoft.VC140.CRT\*" "%TmpDir%\64bit\" || exit /B 1
+xcopy "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\redist\x64\Microsoft.VC140.MFC\*" "%TmpDir%\64bit\" || exit /B 1
+xcopy "C:\Program Files (x86)\Windows Kits\8.1\Debuggers\x64\dbghelp.dll" "%TmpDir%\64bit\" || exit /B 1
+xcopy "C:\Program Files (x86)\Windows Kits\10\Redist\ucrt\DLLs\x64\*" "%TmpDir%\32bit\" || exit /B 1
 xcopy /E /I "%WORKSPACE%\bin\release\help" "%TmpDir%\64bit\help" || exit /B 1
 xcopy /E /I "%WORKSPACE%\bin\release\langs" "%TmpDir%\64bit\langs" || exit /B 1
 
@@ -57,7 +59,11 @@ xcopy "%WORKSPACE%\bin\release\libserializer32u.dll" "%TmpDir%\64bit\ShellExt32\
 xcopy "%WORKSPACE%\bin\release\libstring32u.dll" "%TmpDir%\64bit\ShellExt32\" || exit /B 1
 xcopy "%WORKSPACE%\bin\release\liblogger32u.dll" "%TmpDir%\64bit\ShellExt32\" || exit /B 1
 xcopy "%WORKSPACE%\bin\release\sqlite3_32.dll" "%TmpDir%\64bit\ShellExt32\" || exit /B 1
-xcopy "%VSInstallDirX86%\VC\redist\x86\Microsoft.VC120.CRT\*" "%TmpDir%\64bit\ShellExt32\" || exit /B 1
+
+xcopy "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\redist\x86\Microsoft.VC140.CRT\*" "%TmpDir%\64bit\ShellExt32\" || exit /B 1
+xcopy "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\redist\x86\Microsoft.VC140.MFC\*" "%TmpDir%\64bit\ShellExt32\" || exit /B 1
+xcopy "C:\Program Files (x86)\Windows Kits\8.1\Debuggers\x86\dbghelp.dll" "%TmpDir%\64bit\ShellExt32\" || exit /B 1
+xcopy "C:\Program Files (x86)\Windows Kits\10\Redist\ucrt\DLLs\x86\*" "%TmpDir%\64bit\ShellExt32\" || exit /B 1
 
 cd %TmpDir%
 "C:\Program Files\7-Zip\7z.exe" a -tzip "%OutDir%\ch-portable-%CHTextVersion%.zip" 32bit 64bit
