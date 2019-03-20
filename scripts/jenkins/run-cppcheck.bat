fdb5a2cc5ef5bcb70e2ec65659f1528d333c5faf
ixen <ixen@copyhandler.com>
2019-03-20 22:07:25 +0100

    Changed language standard for cppcheck (CH-340).

--- scripts/jenkins/run-cppcheck.bat
+++ scripts/jenkins/run-cppcheck.bat
@@ -5,4 +5,4 @@ SET PATH=C:\Program Files\Cppcheck;%PATH%
 
 mkdir "CPPCheck"
 
-cppcheck --enable=warning,style,performance,portability,information --suppressions-list=scripts\jenkins\cppcheck-suppress.txt --inline-suppr --library=microsoft_sal.cfg --library=windows.cfg --std=c++11 --xml-version=2 --xml --platform=win32W -i ext -i tests --project=ch.vc140.sln 2>"CPPCheck\cppcheck.xml"
+cppcheck --enable=warning,style,performance,portability,information --suppressions-list=scripts\jenkins\cppcheck-suppress.txt --inline-suppr --library=microsoft_sal.cfg --library=windows.cfg --std=c++17 --xml-version=2 --xml --platform=win32W -i ext -i tests --project=ch.vc140.sln 2>"CPPCheck\cppcheck.xml"
