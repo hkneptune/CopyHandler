ac285315e87e34ee789a46505ac4f32c0df8dd39
ixen <ixen@draknet.eu>
2020-10-23 13:16:54 +0200

    Adjust path detection in jenkins scripts.

--- scripts/jenkins/_get-output-dir.bat
+++ scripts/jenkins/_get-output-dir.bat
@@ -3,9 +3,9 @@
 call "%WORKSPACE%\scripts\jenkins\_get-version.bat" || exit /b 1
 
 if "%CHDetailVersion%" == "0" (
-	SET OutDir=%WORKSPACE%\Output\%CHTextVersion%
+	SET OutDir="%WORKSPACE%\Output\%CHTextVersion%"
 ) else (
-	SET OutDir=%WORKSPACE%\Output\internal\%CHTextVersion%
+	SET OutDir="%WORKSPACE%\Output\internal\%CHTextVersion%"
 )
 
 mkdir "%OutDir%" || exit /b 0
