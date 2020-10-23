d623b2eb18154a46623c3f7b3007f84a049904ea
ixen <ixen@draknet.eu>
2020-10-23 13:26:25 +0200

    Fix output directory detection by jenkins script.

--- scripts/jenkins/_get-output-dir.bat
+++ scripts/jenkins/_get-output-dir.bat
@@ -3,9 +3,11 @@
 call "%WORKSPACE%\scripts\jenkins\_get-version.bat" || exit /b 1
 
 if "%CHDetailVersion%" == "0" (
-	SET OutDir="%WORKSPACE%\Output\%CHTextVersion%"
+	SET "OutDir=%WORKSPACE%\Output\%CHTextVersion%"
 ) else (
-	SET OutDir="%WORKSPACE%\Output\internal\%CHTextVersion%"
+	SET "OutDir=%WORKSPACE%\Output\internal\%CHTextVersion%"
 )
+
+echo "Detected output directory: %OutDir%"
 
 mkdir "%OutDir%" || exit /b 0
