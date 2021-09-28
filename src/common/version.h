e41549848a8cd1aa33f10c38eac1559e2a19ed72
ixen <ixen@draknet.eu>
2021-09-28 10:03:45 +0200

    Bump version to 1.47.

--- src/common/version.h
+++ src/common/version.h
@@ -10,7 +10,7 @@
 
 // Version of program
 #define PRODUCT_VERSION1 1
-#define PRODUCT_VERSION2 45
+#define PRODUCT_VERSION2 47
 #define PRODUCT_VERSION3 0
 #define PRODUCT_VERSION4 0
 
@@ -23,7 +23,7 @@
 	#define PRODUCT_NUMERIC_VERSION STRINGIZE2(PRODUCT_VERSION1) L"." STRINGIZE2(PRODUCT_VERSION2) L"." STRINGIZE2(PRODUCT_VERSION3) L"." STRINGIZE2(PRODUCT_VERSION4)
 #endif
 
-#define PRODUCT_VERSION "1.45internal-current"
+#define PRODUCT_VERSION "1.47internal-current"
 
 #if SETUP_COMPILER != 1
 	#define SHELLEXT_PRODUCT_FULL_VERSION SHELLEXT_PRODUCT_NAME " " PRODUCT_VERSION
@@ -34,7 +34,7 @@
 #endif
 
 // copyright information
-#define COPYRIGHT_INFO	"Copyright (C) 2001-2016 Józef Starosczyk"
+#define COPYRIGHT_INFO	"Copyright (C) 2001-2021 Józef Starosczyk"
 #define PRODUCT_SITE		"http://www.copyhandler.com"
 #define UPDATE_CHECK_LINK_SECURE	"https://www.copyhandler.com/update-check"
 #define UPDATE_CHECK_LINK_NONSECURE	"http://www.copyhandler.com/update-check"
